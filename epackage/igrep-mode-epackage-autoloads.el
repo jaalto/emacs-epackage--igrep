8eb0bdb8151bcb44481443254b7101e22c59be3c
Jari Aalto <jari.aalto@cante.net>
2019-05-25 17:33:49 +0300

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/igrep-mode-epackage-autoloads.el
@@ -0,0 +1,31 @@
+(autoload 'igrep-easy-menu-item                 "igrep" "" nil);;defsubst
+(autoload 'igrep-insinuate                      "igrep" "" t)
+(autoload 'igrep-quote-file-name                "igrep" "" nil);;defsubst
+(autoload 'igrep                                "igrep" "" t)
+(autoload 'igrep-define                         "igrep" "" t 'macro)
+(autoload 'igrep-find                           "igrep" "" t)
+(autoload 'igrep-find-define                    "igrep" "" t 'macro)
+(autoload 'igrep-visited-files                  "igrep" "" t)
+(autoload 'dired-do-igrep                       "igrep" "" t)
+(autoload 'dired-do-igrep-find                  "igrep" "" t)
+(autoload 'Buffer-menu-igrep                    "igrep" "" t)
+(autoload 'igrep-dired-file-current-word        "igrep" "" nil)
+(autoload 'igrep-buffer-file-name-pattern       "igrep" "" nil)
+(autoload 'igrep-dired-directory-file-pattern   "igrep" "" nil)
+(autoload 'igrep-file-directory                 "igrep" "" nil);;defsubst
+(autoload 'igrep-file-pattern                   "igrep" "" nil);;defsubst
+(autoload 'igrep-format-find-command            "igrep" "" nil)
+(autoload 'igrep-default-arg                    "igrep" "" nil 'macro)
+(autoload 'igrep-default-regex                  "igrep" "" nil)
+(autoload 'igrep-default-files                  "igrep" "" nil)
+(autoload 'igrep-prefix                         "igrep" "" nil);;defsubst
+(autoload 'igrep-read-args                      "igrep" "" nil)
+(autoload 'igrep-read-program                   "igrep" "" nil)
+(autoload 'igrep-read-options                   "igrep" "" nil)
+(autoload 'igrep-read-regex                     "igrep" "" nil)
+(autoload 'igrep-insert-default-regex           "igrep" "" t)
+(autoload 'igrep-insert-default-files           "igrep" "" t)
+(autoload 'igrep-default-key                    "igrep" "" nil);;defsubst
+(autoload 'igrep-read-files                     "igrep" "" nil)
+(autoload 'igrep-read-file-name                 "igrep" "" nil)
+(provide 'igrep-mode-epackage-autoloads)
